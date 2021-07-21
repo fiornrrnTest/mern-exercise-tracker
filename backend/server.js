12cb65d68654f32b6992a7c9e03fefdbee0016be
fiornrrn <fiornrrn@gmail.com>
2021-07-21 16:00:45 +0700

    ended tutorial

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,11 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB connected");
-});
+  console.log("MongoDB database connection established successfully");
+})
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
@@ -24,5 +25,5 @@ app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 app.listen(port, () => {
-    console.log("server is running on port 5000");
+    console.log(`Server is running on port: ${port}`);
 });
